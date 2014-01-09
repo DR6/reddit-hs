fdbf5f4b5dee7e72dac1a3578e8805d55f184191
DR6 <darkairblast@gmail.com>
2014-01-09 17:44:11 +0000

    withLogin added

--- Network/Reddit.hs
+++ Network/Reddit.hs
@@ -4,7 +4,7 @@ module Network.Reddit(
 	module Network.Reddit.Instances,
 	module Network.Reddit.Types,
 	module Network.Reddit.Monad,
-	fetchR, actR, withModhash) where
+	fetchR, actR, withModhash, withLogin) where
 
 import Network.Reddit.Monad
 import Network.Reddit.Types
@@ -24,3 +24,8 @@ withModhash m r = iterM useModhash r
 	where useModhash :: RedditF RequiresLogin (Reddit () a) -> Reddit () a
 	      useModhash (Fetch i handler) = fetchR i >>= handler
 	      useModhash a@(Act _ _) = join . liftF $ WithLogin m a
+
+withLogin :: String -> String -> Reddit RequiresLogin a -> Reddit () a
+withLogin u p r = do
+	modhash <- fetchR $ Login u p
+	withModhash modhash r
